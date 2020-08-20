34f880573d47510ce297cd81e13a1b71208d1874
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-20 07:59:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,6 @@
 # call your method here!
 
 def hello_t(array)
-  i = 0
- 
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-  array
-end
-
-def hello_t(array)
   if block_given?
     i = 0
  
